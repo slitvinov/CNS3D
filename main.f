65b77f2cfa5e41d5ff292f60cc9f94940c03f7d2
Sergey Litvinov <slitvinov@gmail.com>
2024-11-13 15:09:51 +0100

    up

--- main.f
+++ main.f
@@ -1,8 +1,8 @@
       COMMON
-     1   /VAR/U(21,18,10),V(21,18,10),P(21,18,10),OK(21,18,10),
+     1   /VAR/U(21,18,10),V(21,18,10),P(21,18,10),DK(21,18,10),
      2     DE(21,18,10),ERRU,ERRV,ERRM,ERRK,ERRE,ERRW,
      3     PP(21,18,10),W(21,18,10),TM(21,18,10),
-     1   /PRCP/ VISE(21,18,10),CEN(21,18,10),VISC,DENIN,FLOWIN,
+     1   /PRCP/ VISE(21,18,10),DEN(21,18,10),VISC,DENIN,FLOWIN,
      1   /PCCR/ CU(21,18,10),DV(21,18,10),DW(21,18,10)
      1   /TUR/ SIGK,SIGE,CMU,C1,C2,CMU1,CMU2,E,CK,HINUM,SMNUM,ANV1(800),
      2     YN(800),YN1(800),SINX(800),SINY(800),SINZ(800),ANW1(800),
@@ -26,9 +26,9 @@
      5     TXZT(21,18,10),TXZB(21,18,10),TZXE(21,18,10),
      6     TZXW(21,18,10),TZYN(21,18,10),TZYS(21,18,10)
       COMMON
-     1     /UNSTDY/U0(21,18,10),V0(21,18,10),W0(21,18,10),OKO(21,18,10),
-     2     CEO(21,18,10),DENO(21,18,10),TMO(21,18,10)
-      LOGICAL INSCU,INSOV,INSOP,INSOK,INSOE,INPRO,INSOW,INSOT
+     1     /UNSTDY/U0(21,18,10),V0(21,18,10),W0(21,18,10),DK0(21,18,10),
+     2     DE0(21,18,10),DEN0(21,18,10),TM0(21,18,10)
+      LOGICAL INS0U,INS0V,INS0P,INS0K,INS0E,INPR0,INS0W,INS0T
 C*****[ INPUT DATA GUIDE ]*********************************************
 C  NLIMT : MAXIMUM NO. OF ITERATIONS LIMIT
 C
@@ -62,9 +62,48 @@ C-----CONSTANTS
       PI=3.141592654
       HINUM=1.E30
       SMNUM=1.E-30
-      INSCU=.TRUE.
-      INSCV=.TRUE.
-      INSCP=.TRUE.
+      INS0U=.TRUE.
+      INS0V=.TRUE.
+      INS0P=.TRUE.
+      CMU1=CMU**0.25
+      CMU2=CMU**0.75
+      INS0U=.TRUE.
+      INS0V=.TRUE.
+      INS0P=.TRUE.
+      INS0K=.TRUE.
+      INS0E=.TRUE.
+      INPR0=.TRUE.
+      INS0W=.TRUE.
+      INS0T=.TRUE.
+      IF(IG .EQ. 2) GO TO 10
+      INS0K=.TRUE.
+      INS0E=.TRUE.
+      INPR0=.TRUE.
+ 10   CONTINUE
+C*****[READ IN INITIAL FLOW FIELDS FROM RESTART FILE (LU = 8)]********
+      READ(8,100) L,M,N,L1,L2,M1,M2,N1,N2
+      L0=L+1
+      M0=M+1
+      N0=N+1
+      LT=L-1
+      MT=M-1
+      NT=N-1
+C-----INITILIZE VARIABLES
+C      CALL INIT
+C-----RESTART FILE
+      DO 50 K=1,N
+         DO 50 I=1,L
+            READ(8,400)
+            DO 50 J=1,M
+               READ(8,500) X(I,J,K),Y(I,J,K),Z(I,J,K),U(I,J,K),V(I,J,K),
+     1              W(I,J,K),P(I,J,K),TM(I,J,K),DK(I,J,K),DE(I,J,K),
+     2              VISCTM,DENTM
+               U0(I,J,K)=U(I,J,K)
+               V0(I,J,K)=V(I,J,K)
+               TM0(I,J,K)=TM(I,J,K)
+               DE0(I,J,K)=DE(I,J,K)
+               DEN0(I,J,K)=DEN(I,J,K)
+ 50         CONTINUE
  999  CONTINUE
  100  FORMAT(9I5)
  200  FORMAT(11F7.4)
